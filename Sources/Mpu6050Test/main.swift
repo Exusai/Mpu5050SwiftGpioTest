2f48c2367e3618ad6696ff1901d699386fa18f8c
Faber <a.a.r.samuel99@gmail.com>
2021-05-17 19:40:06 -0500

    Code updated
    more readable

--- Sources/Mpu6050Test/main.swift
+++ Sources/Mpu6050Test/main.swift
@@ -7,7 +7,7 @@ SCL --- 3 SCL (5)(6) GND --- GND
 */
 //Imports
 import SwiftyGPIO
-import MPU6050
+//import MPU6050
 import Foundation
 
 //Init the MPU6050 and HW
@@ -17,7 +17,7 @@ let i2c = i2cs[1]
 let mp = MPU6050(i2c)
 
 mp.enable(true)
-mp.range(gyroRange: 1, accelRange: 1)
+mp.range(gyroRange: 1, accelRange: 2) // Define los rangos de operación del sensor
 
 //Actual code
 var gyroX: Int = 0
@@ -45,7 +45,7 @@ var yawOut: Double = 0
 var setGyroAngles: Bool = false
 
 /// CALIBRATING
-print("Calibrating")
+print("Calibrando")
 mp.reset()
 mp.enable(true)
 for n in 0...500 {
@@ -63,65 +63,58 @@ gxCal = gxCal / 500
 gyCal = gyCal / 500
 gzCal = gzCal / 500
 print("\n")
-print("gxAverage: \(gxCal) gyAverage: \(gyCal) gzAverage: \(gzCal) ")
-print("Calibrated")
+print("gxProm: \(gxCal) gyProm: \(gyCal) gzProm: \(gzCal) ")
+print("Calibrado")
 
-/// Readings
+// Inicializar tiempo.
 let ferventTempo = FerventTempo()
-//var time = NSDate()
-//var refreshRate: Double = 100
-/// Headers for printing
+
+// Headers
 print("Pitch\tRoll\tYaw\tTemp")
 while(true){
 	let (ax, ay, az, t, gx, gy, gz) = mp.getAll()
 	gyroX = gx - gxCal
 	gyroY = gy - gyCal
 	gyroZ = gz - gzCal
-	//let newTime = NSDate()
-	//let deltaTime = newTime.timeIntervalSince(time as Date)
-    //time = NSDate()
-    let refreshRate = 1 / ferventTempo.Delta
-    //refreshRate = 1 / deltaTime
-	//print("Execution time: \(deltaTime)")
-	//print("Refresh Rate: \(refreshRate)")
-	//time = NSDate()
-	//print("Gyroscope - x:\(gyroZ),y:\(gyroY),z:\(gyroZ)")
-	//print("Gyroscope - x:\(gx),y:\(gy),z:\(gz)")
-	//gyro angle calc
-	//0.0000611 = 1 / (250Hz / 65.5)
-	let constant1: Double  = 1 / (refreshRate / 0.066)
-	pitchAngle += Double(gyroX) * constant1
-	rollAngle += Double(gyroY) * constant1
-	yawAngle += Double(gyroZ) * constant1
+	
+    //let refreshRate = 1 / ferventTempo.Delta
+    let vx = Double(gyroX) / 65.5
+    let vy = Double(gyroY) / 65.5
+    let vz = Double(gyroZ) / 65.5
+    let delta = ferventTempo.Delta
+    
+    // Gyro ang calc
+    pitchAngle += vx * delta
+    rollAngle += vy * delta
+    yawAngle += vz * delta
 
-	//0.000001066 = 0.0000611 * (3.142(PI) / 180degr) apparently the sin func is in rad
-	let constant2: Double  = constant1 * (Double.pi / 180)
-	pitchAngle += rollAngle * sin(Double(gyroZ) *  constant2)
-	rollAngle -= pitchAngle * sin(Double(gyroZ) *  constant2)
-	//print("Pitch: \(pitchAngle)")
-	//print("Roll: \(rollAngle)")
+	// Transferencia de ang
+	let deg2rad: Double  = (Double.pi / 180)
+	pitchAngle += rollAngle * sin(Double(yawAngle) *  deg2rad)
+	rollAngle -= pitchAngle * sin(Double(yawAngle) *  deg2rad)
 
-	//Accel angle calc
+	// Accel ang calc
 	let ax2: Float = Float(ax) * Float(ax)
 	let ay2: Float = Float(ay) * Float(ay)
 	let az2: Float = Float(az) * Float(az)
 	accelVectorMag = Float(sqrt(ax2 + ay2 + az2))
-	pitchAngleAccel = asin(Double(Float(ay)/accelVectorMag)) * 57.29
-	rollAngleAccel = asin(Double(Float(ax)/accelVectorMag)) * -57.29
+	pitchAngleAccel = asin(Double(Float(ay)/accelVectorMag)) * 57.29 // 57.29 = rad2deg
+	rollAngleAccel = asin(Double(Float(ax)/accelVectorMag)) * -57.29 // 57.29 = rad2deg
 
 	//Calibrate Accel angles
 	pitchAngleAccel += 0.1
     rollAngleAccel += 1.8
 
 	if setGyroAngles {
-		pitchAngle = pitchAngle * 0.9 + pitchAngleAccel * 0.1
-		rollAngle = rollAngle * 0.9 + rollAngleAccel * 0.1
+		pitchAngle = pitchAngle * 0.96 + pitchAngleAccel * 0.04
+		rollAngle = rollAngle * 0.96 + rollAngleAccel * 0.04
 	} else {
 		pitchAngle = pitchAngleAccel
 		rollAngle = rollAngleAccel
 		setGyroAngles = true
 	}
-
+    
+    // Filtro complemenatario
 	pitchOut = pitchOut * 0.9 + pitchAngle * 0.1
 	rollOut = rollOut * 0.9 + rollAngle * 0.1
 	yawOut = yawAngle
@@ -131,11 +124,6 @@ while(true){
     let yawString = String(format: "%.2f", yawOut)
     let tempString = String(format: "%.2f", t)
     
-    /*
-	print("Pitch:   \(pitchString)")
-	print("Roll:    \(rollString)")
-	print("Yaw:     \(yawString)")
-    print("Temp:    \(tempString)")*/
     print("\(pitchString)\t\(rollString)\t\(yawString)\t\(tempString)", terminator:"\r")
     
 }
